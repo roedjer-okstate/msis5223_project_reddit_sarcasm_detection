e461bfd96ec341d1caef0e44be634aeb31e737dd
shreyanDC <shreyan.datta_chakraborty@okstate.edu>
2023-04-26 10:24:13 -0500

    Proportion assets + code for descriptive statistics

--- code/Data Sampling - Descriptive Statistics.py
+++ code/Data Sampling - Descriptive Statistics.py
@@ -1,157 +1,230 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[62]:
 
 
 import pandas as pd
 
 
-# In[4]:
+# In[63]:
 
 
 df_X_train_transformed = pd.read_csv('/Users/shreyandattachakraborty/Documents/College Application/Application for Masters/OSU/Study Materials/2nd Semester/MSIS 5223/Assignments/project-deliverable-2-cia/data/predictive_modeling_data/X_train_transformed.csv')
 df_X_train_transformed
 
 
-# In[23]:
+# In[64]:
 
 
 df_X_train_transformed.describe().T
 
 
-# In[6]:
+# In[65]:
+
+
+df_X_train_transformed['subreddit'].value_counts(1)
+
+
+# In[66]:
 
 
 df_Y_train_transformed = pd.read_csv('/Users/shreyandattachakraborty/Documents/College Application/Application for Masters/OSU/Study Materials/2nd Semester/MSIS 5223/Assignments/project-deliverable-2-cia/data/predictive_modeling_data/Y_train_transformed.csv')
 df_Y_train_transformed
 
 
-# In[2]:
+# In[67]:
+
+
+df_Y_train_transformed['label'].value_counts(1)
+
+
+# In[68]:
 
 
 df_X_test_transformed = pd.read_csv('/Users/shreyandattachakraborty/Documents/College Application/Application for Masters/OSU/Study Materials/2nd Semester/MSIS 5223/Assignments/project-deliverable-2-cia/data/predictive_modeling_data/X_test_transformed.csv')
 df_X_test_transformed
 
 
-# In[24]:
+# In[69]:
 
 
 df_X_test_transformed.describe().T
 
 
-# In[7]:
+# In[70]:
 
 
 df_Y_test_transformed = pd.read_csv('/Users/shreyandattachakraborty/Documents/College Application/Application for Masters/OSU/Study Materials/2nd Semester/MSIS 5223/Assignments/project-deliverable-2-cia/data/predictive_modeling_data/Y_test_transformed.csv')
 df_Y_test_transformed
 
 
-# In[5]:
+# In[71]:
 
 
 df_X_val_transformed = pd.read_csv('/Users/shreyandattachakraborty/Documents/College Application/Application for Masters/OSU/Study Materials/2nd Semester/MSIS 5223/Assignments/project-deliverable-2-cia/data/predictive_modeling_data/X_val_transformed.csv')
 df_X_val_transformed
 
 
-# In[25]:
+# In[72]:
 
 
 df_X_val_transformed.describe().T
 
 
-# In[8]:
+# In[73]:
 
 
 df_Y_val_transformed = pd.read_csv('/Users/shreyandattachakraborty/Documents/College Application/Application for Masters/OSU/Study Materials/2nd Semester/MSIS 5223/Assignments/project-deliverable-2-cia/data/predictive_modeling_data/Y_val_transformed.csv')
 df_Y_val_transformed
 
 
-# In[ ]:
+# In[74]:
 
 
+# print('Transformed Data Subr')
+print(df_X_train_transformed['subreddit'].value_counts(1))
+print(df_X_test_transformed['subreddit'].value_counts(1))
+print(df_X_val_transformed['subreddit'].value_counts(1))
 
 
-
-# In[ ]:
-
+# In[95]:
 
 
+df_X_train_transformed['subreddit'].value_counts(1)[0]
 
 
-# In[9]:
+# In[116]:
+
+
+import numpy as np
+df_transformed_temp = pd.DataFrame({'AskReddit subreddit proportion': [df_X_train_transformed['subreddit'].value_counts(1)[0],
+                                          df_X_val_transformed['subreddit'].value_counts(1)[0],
+                                          df_X_test_transformed['subreddit'].value_counts(1)[0]],
+                                    'politics subreddit proportion': [df_X_train_transformed['subreddit'].value_counts(1)[1],
+                                          df_X_val_transformed['subreddit'].value_counts(1)[1],
+                                          df_X_test_transformed['subreddit'].value_counts(1)[1]],
+                                   'worldnews subreddit proportion': [df_X_train_transformed['subreddit'].value_counts(1)[2],
+                                          df_X_val_transformed['subreddit'].value_counts(1)[2],
+                                          df_X_test_transformed['subreddit'].value_counts(1)[2]],
+                                   'Target Variable 1s proportion': [df_Y_train_transformed['label'].value_counts(1)[0],
+                                          df_Y_val_transformed['label'].value_counts(1)[0],
+                                          df_Y_test_transformed['label'].value_counts(1)[0]],
+                                    'Target Variable 0s proportion': [df_Y_train_transformed['label'].value_counts(1)[1],
+                                          df_Y_val_transformed['label'].value_counts(1)[1],
+                                          df_Y_test_transformed['label'].value_counts(1)[1]]}, 
+                                   index=['Training', 'Validation','Test'])
+ax = df_transformed_temp.plot.bar(figsize=(33,14))
+
+for container in ax.containers:
+    ax.bar_label(container)
+
+
+# In[93]:
+
+
+np.random.rand(2)
+
+
+# In[76]:
+
+
+df_X_train_transformed['subreddit'].value_counts(1).plot(kind='barh', figsize=(8, 6))
+
+
+# In[77]:
 
 
 df_X_train_untransformed = pd.read_csv('/Users/shreyandattachakraborty/Documents/College Application/Application for Masters/OSU/Study Materials/2nd Semester/MSIS 5223/Assignments/project-deliverable-2-cia/data/predictive_modeling_data/X_train_untransformed.csv')
 df_X_train_untransformed
 
 
-# In[36]:
+# In[78]:
 
 
 from IPython.display import display
 
 
-# In[44]:
+# In[79]:
 
 
 with pd.option_context('display.max_rows', df_X_train_untransformed.shape[1]):
     display(df_X_train_untransformed.describe().T)
 
 
-# In[10]:
+# In[80]:
 
 
 df_Y_train_untransformed = pd.read_csv('/Users/shreyandattachakraborty/Documents/College Application/Application for Masters/OSU/Study Materials/2nd Semester/MSIS 5223/Assignments/project-deliverable-2-cia/data/predictive_modeling_data/Y_train_untransformed.csv')
 df_Y_train_untransformed
 
 
-# In[11]:
+# In[81]:
 
 
 df_X_test_untransformed = pd.read_csv('/Users/shreyandattachakraborty/Documents/College Application/Application for Masters/OSU/Study Materials/2nd Semester/MSIS 5223/Assignments/project-deliverable-2-cia/data/predictive_modeling_data/X_test_untransformed.csv')
 df_X_test_untransformed
 
 
-# In[46]:
+# In[82]:
 
 
 with pd.option_context('display.max_rows', df_X_test_untransformed.shape[1]):
     display(df_X_test_untransformed.describe().T)
 
 
-# In[12]:
+# In[83]:
 
 
 df_Y_test_untransformed = pd.read_csv('/Users/shreyandattachakraborty/Documents/College Application/Application for Masters/OSU/Study Materials/2nd Semester/MSIS 5223/Assignments/project-deliverable-2-cia/data/predictive_modeling_data/Y_test_untransformed.csv')
 df_Y_test_untransformed
 
 
-# In[13]:
+# In[84]:
 
 
 df_X_val_untransformed = pd.read_csv('/Users/shreyandattachakraborty/Documents/College Application/Application for Masters/OSU/Study Materials/2nd Semester/MSIS 5223/Assignments/project-deliverable-2-cia/data/predictive_modeling_data/X_val_untransformed.csv')
 df_X_val_untransformed
 
 
-# In[48]:
+# In[85]:
 
 
 with pd.option_context('display.max_rows', df_X_val_untransformed.shape[1]):
     display(df_X_val_untransformed.describe().T)
 
 
-# In[14]:
+# In[86]:
 
 
 df_Y_val_untransformed = pd.read_csv('/Users/shreyandattachakraborty/Documents/College Application/Application for Masters/OSU/Study Materials/2nd Semester/MSIS 5223/Assignments/project-deliverable-2-cia/data/predictive_modeling_data/Y_val_untransformed.csv')
 df_Y_val_untransformed
 
 
-# In[ ]:
+# In[117]:
 
 
+import numpy as np
+df_untransformed_temp = pd.DataFrame({'AskReddit subreddit proportion': [df_X_train_untransformed['subreddit'].value_counts(1)[0],
+                                          df_X_val_untransformed['subreddit'].value_counts(1)[0],
+                                          df_X_test_untransformed['subreddit'].value_counts(1)[0]],
+                                    'politics subreddit proportion': [df_X_train_untransformed['subreddit'].value_counts(1)[1],
+                                          df_X_val_untransformed['subreddit'].value_counts(1)[1],
+                                          df_X_test_untransformed['subreddit'].value_counts(1)[1]],
+                                   'worldnews subreddit proportion': [df_X_train_untransformed['subreddit'].value_counts(1)[2],
+                                          df_X_val_untransformed['subreddit'].value_counts(1)[2],
+                                          df_X_test_untransformed['subreddit'].value_counts(1)[2]],
+                                   'Target Variable 1s proportion': [df_Y_train_untransformed['label'].value_counts(1)[0],
+                                          df_Y_val_untransformed['label'].value_counts(1)[0],
+                                          df_Y_test_untransformed['label'].value_counts(1)[0]],
+                                    'Target Variable 0s proportion': [df_Y_train_untransformed['label'].value_counts(1)[1],
+                                          df_Y_val_untransformed['label'].value_counts(1)[1],
+                                          df_Y_test_untransformed['label'].value_counts(1)[1]]}, 
+                                   index=['Training', 'Validation','Test'])
+ax = df_untransformed_temp.plot.bar(figsize=(33,14))
 
+for container in ax.containers:
+    ax.bar_label(container)
 
 
 # In[ ]:
